a03f96656e1b31143b68cb198194fd24d4e1100b
jiwonpg98 <jiwonpg98@gmail.com>
2022-03-17 23:35:16 +0900

    Add(ormpractice): typeorm 설정 추가

---
+++ typeORMPractice/src/config/ormconfig.ts
@@ -0,0 +1,22 @@
+import { ConnectionOptions } from "typeorm";
+
+const ormconfig: ConnectionOptions = {
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASENAME,
+  synchronize: true,
+  logging: false,
+  // entities: ["dist/entity/**/*.js"],
+  // migrations: ["src/migration/**/*.ts"],
+  // subscribers: ["src/subscriber/**/*.ts"],
+  // cli: {
+  //   entitiesDir: "src/entity",
+  //   migrationsDir: "src/migration",
+  //   subscribersDir: "src/subscriber",
+  // },
+};
+
+export default ormconfig;
---
+++ typeORMPractice/src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+// import userRouter from "./routes/user";
+import { createConnection } from "typeorm";
+import connectionOptions from "./config/ormconfig";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// app.use("/user", userRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello!");
+});
+
+createConnection(connectionOptions)
+  .then(() => {
+    console.log("DB CONNECTION!");
+    app.listen(3000, () => {
+      console.log("Server Starting... 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
